a803d08e2de26e6ab6e868e227cd84f039b52df5
Omar C++ enjoyer <128183128+omartheover@users.noreply.github.com>
2025-12-11 02:22:29 +0200

    Update gambling.cpp

--- gambling.cpp
+++ gambling.cpp
@@ -1,25 +1,30 @@
 #include <raylib.h>
 #include <iostream>
 
+
+int gambled_money = 0;
+int output = 0;
+int inventory = 250000;
+
 int main(){
 
-
-    int gambled_money = 0;
-    int output = 0;
 
     InitWindow(900, 900, "mine");
     InitAudioDevice();
     SetTargetFPS(60);
-    Texture2D back = LoadTexture("C:\\Users\\PCM\\Pictures\\bg\\hi3.jpg");
+    Image imag = LoadImage("C:\\Users\\PCM\\Pictures\\bg\\backe.jpg");
+    ImageResize(&imag, 900, 900);
+    Texture2D back = LoadTextureFromImage(imag);
     Sound money = LoadSound("C:\\Users\\PCM\\Pictures\\bg\\mysound.wav");
     Texture2D crusor = LoadTexture("C:\\Users\\PCM\\Pictures\\Camera Roll\\crusor.png");
     Texture2D button = LoadTexture("C:\\Users\\PCM\\Pictures\\Camera Roll\\gamble.png");
+    Texture2D button2 = LoadTexture("C:\\Users\\PCM\\Pictures\\Camera Roll\\gamble2.png");
     Vector2 crusorpos = {100.0f, 100.0f};
     int weight = 50;
     int hight = 50;
     Rectangle sourceRec = { 0.0f, 0.0f, (float)weight, (float)weight };
     Rectangle destRec = { crusorpos.x, crusorpos.y, weight*1.0f, hight*1.0f };
-    Vector2 origin = { (float)weight, (float)hight };
+    Vector2 origin = { (float)weight/2.0f, (float)hight/2.0f };
     int rotation = 0;
 
     int button_x = 310;
@@ -27,30 +32,42 @@ int main(){
     int button_height = 169;
     int button_width = 300;
     Rectangle btnbounds = { (float)button_x, (float)button_y, (float)button_width, (float)button_height };
+    
 
-    Vector2 mouse_place = {0.0f, 0.0f};
 
     while(!WindowShouldClose()){
         HideCursor();
         crusorpos = GetMousePosition();
         destRec = { crusorpos.x, crusorpos.y, weight*1.0f, hight*1.0f };
-        mouse_place = GetMousePosition();
+        Rectangle crusorv = {crusorpos.x, crusorpos.y, 50.0f, 50.0f};
 
         BeginDrawing();
         ClearBackground(WHITE);
         DrawTexture(back, 0, 0, WHITE);
         DrawTexture(button, 310,450,WHITE);
-        if(CheckCollisionPointRec(mouse_place, btnbounds)){
+        if(CheckCollisionRecs(crusorv, btnbounds)){
+            DrawTexture(button2, 310,450,WHITE);
             if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
                 PlaySound(money);
-                 gambled_money = (rand() % 300000) + 1;
-                 output = (rand() % 400000);
+                 gambled_money = (rand() % 250000) + 1;
+                 if(gambled_money > inventory){
+                    continue;
+                 }
+                 inventory = inventory - gambled_money;
+                 output = (rand() % 300000);
+                 inventory = inventory + output;
                 
             }
         }
 
-        DrawText(TextFormat("you have gambled: %d    you got: %d", gambled_money, output), 100, 300, 30, GREEN);
-        DrawText(TextFormat("Aint no way this C++ task took me 3 hours bruh"), 100, 200, 30, RED);
+        
+        DrawText(TextFormat("You have gambled: %d$", gambled_money), 300, 170, 32, YELLOW);
+        if(output > gambled_money){
+            DrawText(TextFormat("You earned: %d$", output), 330, 220, 32, GREEN);
+        }
+        else{
+            DrawText(TextFormat("You earned: %d$", output), 330, 220, 32, RED);}
+        DrawText(TextFormat("Wallet: %d$", inventory), 330, 270, 32, GOLD);
         DrawTexturePro(crusor, sourceRec, destRec, origin, (float)rotation, WHITE);
 
         EndDrawing();
